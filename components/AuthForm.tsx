c69ed7e5d70ff481df0543645e05b0b0c23349a3
Whisper4 <nghiahh2004@gmail.com>
2025-03-31 21:06:02 +0700

    Implement admin & client firebase

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,9 @@ import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword} from "@firebase/auth";
+import {auth} from "@/firebase/client";
+import {SignUp} from "@/lib/actions/auth.action";
 
 
 const authFormSchema = (type: FormType) => {
@@ -39,6 +42,17 @@ const AuthForm = ({type} : {type: FormType}) => {
     function onSubmit(values: z.infer<typeof formSchema>) {
         try{
             if(type === "sign-up") {
+                const {name, email, password} = values;
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+                const result = await SignUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password
+                });
+                if(!result.success){
+                    toast.error(result.message);
+                }
                 toast.success("Account created successfully.")
                 router.push("/sign-in")
             }
